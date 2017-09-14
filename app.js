64ffae873c95f1bde930147436e4133bc459aa87
Yixiong <xiongyenihao@hotmail.com>
2017-09-14 21:57:53 +0800

    redirect http to https

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ var https = require('https');
 var path = require('path');
 var fs = require('fs');
 
-var app = express();
-
 var options = {
   key: fs.readFileSync('./214260419960706.key'),
   cert: fs.readFileSync('./214260419960706.pem')
 };
 
+var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
-console.log("Listening 80...");
 console.log("Listening 443...");
+
+http.createServer(function (req, res) {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(80);
+console.log("Listening 80 and redirecting to 443...");
