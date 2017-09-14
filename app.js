dcbfdcb5cf41b20ab1e22986836d11293054966f
Yixiong <xiongyenihao@hotmail.com>
2017-09-14 20:34:01 +0800

    enable https

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var http = require('http');
+var https = require('https');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
+var options = {
+  key: fs.readFileSync('./214260419960706.key'),
+  cert: fs.readFileSync('./214260419960706.pem')
+};
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = '80';
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, ()=>console.log("Running..."));
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+console.log("Listening 80...");
+console.log("Listening 443...");
