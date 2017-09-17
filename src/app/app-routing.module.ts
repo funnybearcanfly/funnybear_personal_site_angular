e2a394a12b3666a7833d3f32c1fae6ffbcecda06
Yixiong <xiongyenihao@hotmail.com>
2017-09-17 20:56:28 +0800

    add about component and timeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AboutComponent } from './about.component';
 import { PostsComponent } from './posts.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/posts', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
     { path: 'posts', component: PostsComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about.component';
 import { PostsComponent } from './posts.component';
 
 import { AppRoutingModule } from './app-routing.module'
@@ -13,6 +14,7 @@ import { PostService } from './post.service';
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
     PostsComponent
   ],
   imports: [
